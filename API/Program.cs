b0815f4e150a9e201a8891a528661933ede008aa
NitinApte <nitinnapte@gmail.com>
2025-07-05 23:49:12 -0400

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -11,9 +11,13 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000", "https://localhost:3000/"));
+
 app.MapControllers();
 
 using var scope = app.Services.CreateScope();
@@ -30,6 +34,5 @@ catch (Exception ex)
     var logger = services.GetRequiredService<ILogger<Program>>();
     logger.LogError(ex, "An error ouccred during migration.");
 }
-
 
 app.Run();
