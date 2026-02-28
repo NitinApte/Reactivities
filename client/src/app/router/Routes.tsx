72722988d734314a8b258eefb18435a24bf5d4bc
NitinApte <nitinnapte@gmail.com>
2026-02-27 20:50:31 -0500

    End of section 8

--- client/src/app/router/Routes.tsx
+++ client/src/app/router/Routes.tsx
@@ -1,20 +1,20 @@
 import { createBrowserRouter } from "react-router";
 import App from "../layout/App";
 import HomePage from "../../features/home/HomePage";
-import ActivityDashboard from "../../features/activities/Dashboard/ActivityDashboard";
+import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 import ActivityForm from "../../features/activities/form/ActivityForm";
-import ActivityDetails from "../../features/activities/details/ActivityDetails";
+import ActivityDetailPage from "../../features/activities/details/ActivityDetailsPage";
 
 export const router = createBrowserRouter([
-    {
-        path : '/',
-        element : <App />,
-        children: [
-            {path: '', element: <HomePage/>},
-            {path: 'activities', element: <ActivityDashboard/>},
-            {path: 'activities/:id', element: <ActivityDetails/>},
-            {path: 'createActivity', element: <ActivityForm key='create'/>},
-            {path: 'manage/:id', element: <ActivityForm/>},
-        ]
-    }
-])
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "", element: <HomePage /> },
+      { path: "activities", element: <ActivityDashboard /> },
+      { path: "activities/:id", element: <ActivityDetailPage /> },
+      { path: "createActivity", element: <ActivityForm key="create" /> },
+      { path: "manage/:id", element: <ActivityForm /> },
+    ],
+  },
+]);
